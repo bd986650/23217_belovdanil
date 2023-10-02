7d15d8139c2ae618f96520cd9f8b6b30a06fe81e
danil belov <96487423+bd986650@users.noreply.github.com>
2023-10-02 08:48:15 +0700

    Update 2week.hs

--- 2week.hs
+++ 2week.hs
@@ -1,28 +1,13 @@
 -- 1 Task
-f1 :: Double -> Double
-f1 x = x
-
-g1 :: Double -> Double
-g1 x = x
-
 p1 :: (Double -> Double) -> (Double -> Double) -> Double -> Double
-p1 f1 g1 p | f1 p == g1 p = p
-           | f1 p > g1 p = f1 p
-           | otherwise = g1 p
+p1 f g p = max (f p) (g p)
 
 -- 2 Task
-exp' :: Double -> Double
-exp' = exp
-
 p2 :: (Double -> Double) -> Double -> Double
-p2 exp' p | exp' p > p = exp' p
-         | exp' p == p = p
-         | otherwise = p
-
+p2 f p = p1 f exp p
 
 -- 3 Task
-f3 :: Double -> Double
-f3 x = x
-
 p3 :: (Double -> Double) -> Double -> Double -> Double
-p3 f3 p n = if n == 1 then f3 p else n * (f3 p)
+p3 f p n | n < 0 = error "Error"
+          | n == 0 = p
+          | otherwise = p3 f (f p) (n - 1)
